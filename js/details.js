ce99441384e4b17b6b2b269519f3b7c3dcb0d328
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-04-29 14:33:27 +0200

    update

--- js/details.js
+++ js/details.js
@@ -1,40 +1,47 @@
 
 const details = document.querySelector (".details");
+const idElement = document.querySelector (".id");
+const queryString = document.location.search;
 
-const querystring = document.location.search;
-
-const params =  URLSearchParams(queryString);
+const params =  new URLSearchParams(queryString);
 
 const id = params.get("id");
 
 console.log(id);
 
-const url ="https://breakingbadapi.com/api/characters/"; 
-
+const url =`https://breakingbadapi.com/api/characters/${id}`; 
 
 console.log(url);
 
+let payload ={ };
+
+function creatHtml(){
+  console.log({payload,id});
+if (Object.keys(payload).length){
+  idElement.innerText = id;
+details.innerHTML = "";
+  details.innerHTML += `<h1>${payload.name}</h1>
+  <p>${payload.occupation[0]}<span class="id"></span></p>`
+
+}
+
+                                                    
+}
 
 async function fetchApi(){
 
   try{
     const response = await fetch(url);
-    const  details = await response.json();
-    console.log(details);
+    const data = await response.json();
+    console.log({data});
+    payload = {...data[0]};
+    creatHtml();
   }
   catch(error){
     details.innerHTML = message("error", error)
   }
 }
 
-fetchApi()
-
-function creatHtml(details){
-
-  details.innerHTML += `<h1>${details.name}</h1>
-                              <p>${details.occupation}<span class="id"></span></p>`
+ fetchApi()
 
 
-                         
-
-}
--- js/index.js
+++ js/index.js
@@ -1,14 +1,41 @@
-const url = "https://breakingbadapi.com/api/characters/";
 
-const options = document.querySelector(".details")
+const selectElement = document.querySelector("#number_element");
 
-  async function MakeApiCall(){
-    const response = await fetch(url);
-    const details = await response.json();
+console.log({selectElement});
 
-    console.log(details); 
+selectElement.addEventListener("change",function(event){
+  const value = parseInt(event.target.value);
+
+  const list = document.querySelector(".list");
+
+  list.innerHTML = "";
+
+  for(let i=1; i<=value; i++) {
+    list.innerHTML += `<a class="item" href="details.html?id=${i}">${i}</a>`
+
+  }
+  
+  //console.log({value})
+
+})
+
+
+// const url  ="https://breakingbadapi.com/api/characters/"; 
+// const options = document.querySelector(".details")
+
+//   async function MakeApiCall(){
+//     const response = await fetch(url);
+//     const details = await response.json();
+
+//     console.log(details); 
+
+
+//     for (let i = 0; i < details.length; i++) {
+//       console.log(details[i].name);
+      
+//     }
 
   
-}
+// }
 
-MakeApiCall()
+// MakeApiCall()
